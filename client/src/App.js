384616f158cc0c7cb0dac187c8f64968aa963137
Munna-Khandakar <munnashisad@gmail.com>
2022-05-31 11:22:45 +0600

    pages folder initiated

--- client/src/App.js
+++ client/src/App.js
@@ -10,27 +10,29 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { useContext } from "react";
 import { themeContext } from "./Context";
+import Home from "./pages/Home";
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div
-      className="App"
-      style={{
-        background: darkMode ? "black" : "",
-        color: darkMode ? "white" : "",
-      }}
-    >
-      <Navbar />
-      <Intro />
-      <Courses />
-      <Experience />
-      <Workshop />
-      <Portfolio />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
+    // <div
+    //   className="App"
+    //   style={{
+    //     background: darkMode ? "black" : "",
+    //     color: darkMode ? "white" : "",
+    //   }}
+    // >
+    //   <Navbar />
+    //   <Intro />
+    //   <Courses />
+    //   <Experience />
+    //   <Workshop />
+    //   <Portfolio />
+    //   <Testimonial />
+    //   <Contact />
+    //   <Footer />
+    // </div>
+    <Home />
   );
 }
 
